b19389447271937f34fe66c216d887786d217b37
manas-arvindh-yash <aiproject1112@gmail.com>
2025-11-21 15:48:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def set_background(image_path):
         """,
         unsafe_allow_html=True
     )
-set_background("bckgrnd.png")
+set_background("Background.png")
 
 @st.cache_data
 def load_data():
