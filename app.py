85a2fc1ed4e25ecb8630eee23001fb54b4c9c39b
manas-arvindh-yash <aiproject1112@gmail.com>
2025-12-08 20:54:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import base64
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import LabelEncoder
-name = st.text_input("Enter Your Name")
+
 
 st.set_page_config(page_title="ScoreScope", layout="wide")
 st.markdown("""
