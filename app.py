1ef43cff439069e3295e97f6a7931fa73d2464a4
manas-arvindh-yash <aiproject1112@gmail.com>
2025-12-11 06:07:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.markdown("""
         border: 1.5px solid #555 !important;    /* dark grey outline */
         border-radius: 8px !important;
         font-weight: 500 !important;
-        font-size: 12px !important;
+        font-size: 18px !important;
     }
 
     /* ===== DROPDOWN MENU OPTIONS ===== */
